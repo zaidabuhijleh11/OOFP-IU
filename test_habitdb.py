ad84ea931672e13b5fbe11d8c5cfa035f23218d2
zaidabuhijleh11 <zaidabuhijleh61@gmail.com>
2024-11-16 20:32:02 +0300

    Update and rename test_habitdd.py to test_habitdb.py

--- test_habitdb.py
+++ test_habitdb.py
@@ -1,15 +1,15 @@
-from datetime import  datetime , date
+from datetime import  datetime ,date
 import pytest
 from habitDB import HabitDB
 from habit import Habit
-
+import pickle
+from Dates_Persistence import save_dates , load_dates
 
 class TestHabitDB:
 
     @pytest.fixture
     def test_habit(self):
-        test_habit = Habit(name='zaid.n', period='daily', description='testing', streak=0,
-                           broken_count=0, status='incomplete', created_at=datetime.now().date() , duration=100 , day_week="day" )
+        test_habit = Habit(name='zaidabuhijleh', period='daily', description='testing', streak=0, broken_count=0, status='incomplete', created_at=datetime.now().date(), duration=100,day_week="day")
         return test_habit
 
     @pytest.fixture
@@ -21,21 +21,25 @@ class TestHabitDB:
     def db(self):
         return HabitDB()
 
-
-
     @pytest.fixture(autouse=True)
-    def clean_db(self , db ):
+    def clean_db(self, db):
+        db.delete_all_habits()
         yield
         db.delete_all_habits()
+
+    @pytest.fixture(autouse=True)
+    def clean_pickle_files(self):
+            with open('zaid_dates.pkl', 'wb') as file:
+                pickle.dump([], file)
 
     def test_habit_exists(self , db , test_habit1):
         db.create_habit(name=test_habit1.name, period='daily', description='testing', duration=100, day_week='day')
         habit_exists = db.habit_exists(test_habit1.name)
         assert habit_exists == True
-        assert db.habit_exists("zaidferer") == False
+        assert db.habit_exists("zaid abu ") == False
 
-    def test_create_habit(self, test_habit , db):
-        name = 'zaid abuhijleh12 '
+    def test_create_habit(self,db):
+        name = 'zaid'
         habits = db.get_habits()
         habits_count = len(habits)
         db.create_habit(name=name, period='daily', description='testing', duration=100, day_week='day')
@@ -53,9 +57,13 @@ class TestHabitDB:
     def test_get_completed_habits(self, test_habit1 ,db):
         initial = db.get_completed_habits()
         db.create_habit(name=test_habit1.name, period='daily', description='testing', duration=5, day_week='day')
-        test_habit1.completed_dates = [date(2024, 10, 28), date(2024, 10, 29), date(2024, 10, 30),
-                                       date(2024, 10, 31), date(2024, 11, 1), date(2024, 11, 2)]
-        db.update_status_streak(test_habit1,test_habit1.name)
+        save_dates(test_habit1.name, datetime(2024, 11, 1).date())
+        save_dates(test_habit1.name, datetime(2024, 11, 2).date())
+        save_dates(test_habit1.name, datetime(2024, 11, 3).date())
+        save_dates(test_habit1.name, datetime(2024, 11, 4).date())
+        save_dates(test_habit1.name, datetime(2024, 11, 5).date())
+        save_dates(test_habit1.name, datetime(2024, 11, 6).date())
+        db.update_status_streak(test_habit1.name)
         completed = db.get_completed_habits()
         assert test_habit1.name in completed
         assert len(completed) == len(initial) + 1
@@ -64,10 +72,13 @@ class TestHabitDB:
     def test_get_incomplete_habits(self, test_habit1 , db ):
         initial = db.get_incomplete_habits()
         db.create_habit(name=test_habit1.name, period='daily', description='testing', duration=100, day_week='day')
-        test_habit1.completed_dates = [date(2024, 10, 28), date(2024, 10, 29), date(2024, 10, 30),
-                                       date(2024, 10, 31), date(2024, 11, 1), date(2024, 11, 2)]
-
-        db.update_status_streak(test_habit1, test_habit1.name)
+        save_dates(test_habit1.name, datetime(2024, 11, 1).date())
+        save_dates(test_habit1.name, datetime(2024, 11, 2).date())
+        save_dates(test_habit1.name, datetime(2024, 11, 3).date())
+        save_dates(test_habit1.name, datetime(2024, 11, 4).date())
+        save_dates(test_habit1.name, datetime(2024, 11, 5).date())
+        save_dates(test_habit1.name, datetime(2024, 11, 6).date())
+        db.update_status_streak( test_habit1.name)
         incomplete = db.get_incomplete_habits()
         assert test_habit1.name in [habit for habit in incomplete]
         assert len(incomplete) == len(initial) + 1
@@ -76,15 +87,18 @@ class TestHabitDB:
 
     def test_get_streak(self, test_habit1 , db ):
         db.create_habit(name=test_habit1.name, period='daily', description='testing', duration=6, day_week='day')
-        test_habit1.completed_dates = [date(2024, 10, 28), date(2024, 10, 29), date(2024, 10, 30), date(2024, 10, 31),
-                                       date(2024, 11, 1), date(2024, 11, 2)]
-        db.update_status_streak(test_habit1, test_habit1.name)
+        save_dates(test_habit1.name, datetime(2024, 11, 1).date())
+        save_dates(test_habit1.name, datetime(2024, 11, 2).date())
+        save_dates(test_habit1.name, datetime(2024, 11, 3).date())
+        save_dates(test_habit1.name, datetime(2024, 11, 4).date())
+        save_dates(test_habit1.name, datetime(2024, 11, 5).date())
+        save_dates(test_habit1.name, datetime(2024, 11, 6).date())
+        db.update_status_streak( test_habit1.name)
         ex_outcome = db.get_streak(test_habit1.name)
         assert ex_outcome == 5
 
-    def test_get_habits(self, test_habit, test_habit1 , db ):
+    def test_get_habits(self,test_habit, test_habit1 , db ):
         initial = db.get_habits()
-
         db.create_habit(name=test_habit.name, period='daily', description='testing', duration=6, day_week='day')
         db.create_habit(name=test_habit1.name, period='daily', description='testing', duration=6, day_week='day')
         habits = db.get_habits()
@@ -96,11 +110,11 @@ class TestHabitDB:
         db.create_habit(name=test_habit1.name, period='daily', description='testing', duration=6, day_week='day')
         initial_desc = db.get_description(test_habit1.name)
         assert initial_desc == 'testing'
-        db.update_description(test_habit1, 'zaid abuhijleh', test_habit1.name)
+        db.update_description(test_habit1.name , 'new desc')
         new_desc = db.get_description(test_habit1.name)
-        assert new_desc == 'zaid abuhijleh'
+        assert new_desc == 'new desc'
         assert new_desc != initial_desc
-        assert test_habit1.description == 'zaid abuhijleh'
+
 
 
 
@@ -108,42 +122,54 @@ class TestHabitDB:
     def test_update_period(self, test_habit1 , db ):
         db.create_habit(name=test_habit1.name, period='daily', description='testing', duration=6, day_week='day')
         initial_period = db.get_period(test_habit1.name)
-        db.update_period(test_habit1, test_habit1.name , 10 , 'day' )
+        db.update_period( test_habit1.name , 10 , 'day' )
         new_period = db.get_period(test_habit1.name)
         assert new_period == 'weekly'
         assert new_period != initial_period
-        assert test_habit1.period == 'weekly'
+
 
 
     def test_update_streak(self, test_habit1 , db ):
         db.create_habit(name=test_habit1.name, period='daily', description='testing', duration=10, day_week='day')
-        test_habit1.completed_dates = [date(2024, 10, 23), date(2024, 10, 26), date(2024, 10, 28), date(2024, 10, 29),
-                                       date(2024, 10, 30),
-                                       date(2024, 10, 31), date(2024, 11, 1), date(2024, 11, 2), date(2024, 11, 3),
-                                       date(2024, 11, 4)]
-        db.update_status_streak(test_habit1, test_habit1.name)
-        assert test_habit1.streak == 7
+        save_dates(test_habit1.name, datetime(2024, 11, 1).date())
+        save_dates(test_habit1.name, datetime(2024, 11, 2).date())
+        save_dates(test_habit1.name, datetime(2024, 11, 3).date())
+        save_dates(test_habit1.name, datetime(2024, 11, 4).date())
+        save_dates(test_habit1.name, datetime(2024, 11, 5).date())
+        save_dates(test_habit1.name, datetime(2024, 11, 6).date())
+        db.update_status_streak(test_habit1.name)
+        streak = db.get_streak(test_habit1.name)
+        assert streak == 5
 
-    def test_update_streak2(self, test_habit1 , db ):
+    def test_update_streak2(self, db,test_habit1  ):
         db.create_habit(name=test_habit1.name, period='daily', description='testing', duration=10, day_week='day')
-        test_habit1.completed_dates = [
-            date(2024, 10, 1), date(2024, 10, 2), date(2024, 10, 3), date(2024, 10, 4),
-            date(2024, 10, 5), date(2024, 10, 6), date(2024, 10, 7), date(2024, 10, 8),
-            date(2024, 10, 9), date(2024, 10, 10), date(2024, 10, 11)]
-        db.update_status_streak(test_habit1, test_habit1.name)
-        assert test_habit1.streak == 10
-        assert test_habit1.status == 'completed'
+        save_dates(test_habit1.name, datetime(2024, 11, 1).date())
+        save_dates(test_habit1.name, datetime(2024, 11, 2).date())
+        save_dates(test_habit1.name, datetime(2024, 11, 3).date())
+        save_dates(test_habit1.name, datetime(2024, 11, 4).date())
+        save_dates(test_habit1.name, datetime(2024, 11, 5).date())
+        save_dates(test_habit1.name, datetime(2024, 11, 6).date())
+        save_dates(test_habit1.name, datetime(2024, 11, 7).date())
+        save_dates(test_habit1.name, datetime(2024, 11, 8).date())
+        save_dates(test_habit1.name, datetime(2024, 11, 9).date())
+        save_dates(test_habit1.name, datetime(2024, 11, 10).date())
+        save_dates(test_habit1.name, datetime(2024, 11, 11).date())
+        db.update_status_streak(test_habit1.name)
+        streak = db.get_streak(test_habit1.name)
+        completed_habit = db.get_completed_habits()
+        assert streak == 10
+        assert test_habit1.name in completed_habit
 
-    def test_update_streak3(self, test_habit1 , db ):
+    def test_update_streak3(self, db,test_habit1  ):
         db.create_habit(name=test_habit1.name, period='daily', description='testing', duration=10, day_week='day')
-        test_habit1.completed_dates = [
-            date(2024, 10, 1), date(2024, 10, 3), date(2024, 10, 4),
-            date(2024, 10, 5), date(2024, 10, 7), date(2024, 10, 8),
-            date(2024, 10, 9), date(2024, 10, 11)]
-        db.update_status_streak(test_habit1, test_habit1.name)
-
-        assert test_habit1.broken_count == 3
-        assert test_habit1.status == 'incomplete'
+        save_dates(test_habit1.name, datetime(2024, 11, 1).date())
+        save_dates(test_habit1.name, datetime(2024, 11, 2).date())
+        save_dates(test_habit1.name, datetime(2024, 11, 3).date())
+        save_dates(test_habit1.name, datetime(2024, 11, 5).date())
+        save_dates(test_habit1.name, datetime(2024, 11, 6).date())
+        db.update_status_streak(test_habit1.name)
+        broken_count_post = db.get_broken_count(test_habit1.name)
+        assert broken_count_post  == 1
 
     def test_delete_all_habits(self , db , test_habit1 , test_habit):
         db.create_habit(name=test_habit1.name, period='daily', description='testing', duration=10, day_week='day')
@@ -174,16 +200,16 @@ class TestHabitDB:
         db.create_habit(name=test_habit1.name, period='daily', description='testing', duration=10, day_week='day')
         db.delete_all_habits()
         with pytest.raises(ValueError):
-            db.update_description(test_habit1,'zaid ', name=test_habit1.name )
+            db.update_description(test_habit1.name , 'zaid ' )
 
     def test_update_period_of_non_existing_habit(self , db , test_habit1 ):
         db.create_habit(name=test_habit1.name, period='daily', description='testing', duration=10, day_week='day')
         db.delete_all_habits()
         with pytest.raises(ValueError):
-            db.update_period(test_habit1,test_habit1.name)
+            db.update_period(test_habit1.name , 10 , 'day')
 
     def test_update_streak_status_non_existing_habit(self , db , test_habit1 ):
         db.create_habit(name=test_habit1.name , period='daily', description='testing', duration=10, day_week='day')
         db.delete_all_habits()
         with pytest.raises(ValueError):
-            db.update_status_streak(test_habit1, test_habit1.name)
+            db.update_status_streak( test_habit1.name)
