f7082e7c0effbb23778c25e5982446772b9ad13e
zaidabuhijleh11 <zaidabuhijleh61@gmail.com>
2024-11-16 20:35:19 +0300

    Create Dates_Persistence.py

---
+++ Dates_Persistence.py
@@ -0,0 +1,21 @@
+import pickle
+def save_dates(name, date):
+    file_name = f"{name}_dates.pkl"
+    try:
+        with open(file_name, 'rb') as file:
+            done_dates = pickle.load(file)
+    except FileNotFoundError:
+        done_dates = []
+    done_dates.append(date)
+    with open(file_name, 'wb') as file:
+        pickle.dump(done_dates, file)
+
+
+def load_dates(name):
+    file_name = f"{name}_dates.pkl"
+    try:
+        with open(file_name, 'rb') as file:
+            done_dates = pickle.load(file)
+        return done_dates
+    except FileNotFoundError:
+        return []
