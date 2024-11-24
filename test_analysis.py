999fc6bf25912b411f57857a9437f7815b498b0e
zaidabuhijleh11 <zaidabuhijleh61@gmail.com>
2024-11-24 15:01:27 +0300

    Update test_analysis.py

--- test_analysis.py
+++ test_analysis.py
@@ -1,12 +1,23 @@
-from datetime import datetime 
+from datetime import datetime
 from habit import Habit
 from habitDB import HabitDB
 import pytest
 from analysis import get_most_consistent_habits, get_most_struggled_habits, get_daily_habits, get_weekly_habits , calculate_completion_rate
-import pickle
-from Dates_Persistence import  save_dates
+from Dates_Persistence import  save_dates , delete_dates
 
 class TestAnalysis:
+    """ in this class we will test the analysis file
+    pytest.fixtures:
+         test_habit1 : Habit instance used for calling methods from habit and is used as a test habit
+         db: HabitDB instance used to call methods from HabitDB
+         clean_db: cleans the database after and before each test
+         clean_pickle_files: deletes a pickle  file before and after each test
+    methods:
+         test_most_consistent_habit: testing if the program can correctly identify the most consistent habits
+         test_most_struggling_habit: testing if the program can correctly identify the most struggling habits
+         test_weekly_habits: testing if the program can correctly filter habits based on period and return  a list of weekly habits
+         test_daily_habits: testing if the program can correctly filter habits based on period and return  a list of daily habits
+         test_completion_rate: testing if the program can correctly calculate the completion rate """
 
     @pytest.fixture
     def test_habit(self):
@@ -33,16 +44,22 @@ class TestAnalysis:
 
     @pytest.fixture(autouse=True)
     def clean_pickle_files(self):
-        with open('zaid_dates.pkl', 'wb') as file:
-            pickle.dump([], file)
+        delete_dates('zaid')
+        yield
+        delete_dates('zaid')
+
 
     @pytest.fixture(autouse=True)
     def clean_pickle_files1(self):
-        with open('zaidabuhijleh_dates.pkl', 'wb') as file:
-            pickle.dump([], file)
+        delete_dates('zaidabuhijleh')
+        yield
+        delete_dates('zaidabuhijleh')
+
 
 
     def test_get_most_consistent_habits(self , db  , test_habit , test_habit1):
+        """testing by creating 2 habits with a duration of 100 (one habit with streak of 5 and one with 0 since its broken)
+        the result should be a list of names [test_habit1.name , test_habit] since we are sorting based on least broken streak count"""
         db.create_habit(name=test_habit1.name, period='daily', description='testing', duration=100, day_week='day')
         db.create_habit(name=test_habit.name, period='daily', description='testing', duration=100, day_week='day')
         save_dates(test_habit1.name, datetime(2024, 11, 1).date())
@@ -64,8 +81,10 @@ class TestAnalysis:
 
 
     def test_get_most_struggled_habits(self , db  , test_habit , test_habit1):
+        """testing by creating 2 habits with a duration of 100 (one habit with streak of 5 and one with 0 since its broken)
+                the result should be a list of names [test_habit.name , test_habit1.name] since we are sorting based on largest broken streak count"""
         db.create_habit(name=test_habit.name, period='daily', description='testing', duration=100, day_week='day')
-        db.create_habit(name=test_habit1.name, period='daily', description='testing', duration=100, day_week='day')
+        db.create_habit(name=test_habit1.name, period='daily', description='fgds', duration=100, day_week='day')
         save_dates(test_habit1.name, datetime(2024, 11, 1).date())
         save_dates(test_habit1.name, datetime(2024, 11, 2).date())
         save_dates(test_habit1.name, datetime(2024, 11, 3).date())
@@ -86,6 +105,7 @@ class TestAnalysis:
 
 
     def test_weekly_habits(self, db, test_habit1, test_habit):
+        """testing by creating 2 habits one daily and one weekly , it should return a list of daily habits names  """
         db.create_habit(name=test_habit.name, period='weekly', description='testing', duration=100, day_week='day')
         db.create_habit(name=test_habit1.name, period='daily', description='testing', duration=100, day_week='day')
         result = get_weekly_habits(db)
@@ -94,6 +114,7 @@ class TestAnalysis:
 
 
     def test_daily_habits(self, db, test_habit1, test_habit):
+        """testing by creating 2 habits one daily and one weekly , it should return a list of weekly habits names  """
         db.create_habit(name=test_habit.name, period='weekly', description='testing', duration=100, day_week='day')
         db.create_habit(name=test_habit1.name, period='daily', description='testing', duration=100, day_week='day')
         result = get_daily_habits(db)
@@ -103,17 +124,19 @@ class TestAnalysis:
 
 
     def test_completion_rate(self ,db , test_habit , test_habit1):
-        db.create_habit(name=test_habit1.name, period='weekly', description='testing', duration=5, day_week='day')
+        """ testing if the method returns a correct  completion rate by calculating
+         by creating 2 habits and setting one as completed and the other as incomplete
+         the rate should be 50%"""
+        db.create_habit(name=test_habit1.name, period='daily', description='testing', duration=5, day_week='day')
         save_dates(test_habit1.name, datetime(2024, 11, 1).date())
         save_dates(test_habit1.name, datetime(2024, 11, 2).date())
         save_dates(test_habit1.name, datetime(2024, 11, 3).date())
         save_dates(test_habit1.name, datetime(2024, 11, 4).date())
         save_dates(test_habit1.name, datetime(2024, 11, 5).date())
         save_dates(test_habit1.name, datetime(2024, 11, 6).date())
-        db.update_status_streak( test_habit1.name)
-        rate1 = calculate_completion_rate(db)
+        db.update_status_streak(test_habit1.name)
+        rate1 = calculate_completion_rate(db)# 100%
         db.create_habit(name=test_habit.name, period='daily', description='testing', duration=100, day_week='day')
-        rate2 = calculate_completion_rate(db)
+        rate2 = calculate_completion_rate(db)#50%
         assert rate2== '50.0%'
         assert rate1 != rate2
-
