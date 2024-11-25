a8a47091da277138ed6e834b2417cc5a9ed2058a
zaidabuhijleh11 <zaidabuhijleh61@gmail.com>
2024-11-25 13:01:10 +0300

    Update habitDB.py

--- habitDB.py
+++ habitDB.py
@@ -264,6 +264,7 @@ class HabitDB:
             return habits if habits else []
 
 
+
     def delete_all_habits(self) -> None:
         """deletes all habits and records from Table """
         with self.connection as connection:
@@ -381,4 +382,3 @@ class HabitDB:
                 if broken_count > self.get_broken_count(name):
                     cursor.execute(""" UPDATE habits SET streak = 0 ,broken_count= ? WHERE name = ?""", (broken_count, name))
                     connection.commit()
-
